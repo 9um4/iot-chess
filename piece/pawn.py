fe0c9b4f7ac716acd6131f8923cddc5d153aae6b
9um4 <51898564+9um4@users.noreply.github.com>
2023-11-26 19:56:01 +0900

    fix: movement refactoring

--- piece/pawn.py
+++ piece/pawn.py
@@ -1,5 +1,7 @@
 from modi import MODI
 
+from typing import Dict
+
 if __name__ == "__main__":
     from piece import Piece
 else:
@@ -11,12 +13,16 @@ class Pawn(Piece):
         super().__init__(bundle, column_time, degree_time)
         self.is_moved_before: bool = False
 
-    def move(self, movement: int = 1):
-        if (not self.is_moved_before):
-            if (movement == 1):
-                self.move_forward()
-            elif (movement == 2):
-                self.move_forward(2)
-            self.is_moved_before = True
-        else:
-            self.move_forward()
+        self.movement: Dict[int, Dict[str, any]] = {
+            1: {
+                "function": lambda: self.move_forward(1),
+                "expression": "한 칸 앞으로 움직입니다."
+            },
+            2: {
+                "function": lambda: self.move_forward(1) if self.is_moved_before else self.move_forward(2) ,
+                "expression": "과거에 움직이지 않은 경우, 두 칸 앞으로 움직입니다."
+            }
+        }
+
+    def move(self, movement_index: int = 1):
+        self.movement[movement_index]["function"]

c1324b4b1692c725b71f027b28b4b8f3da9de45e
9um4 <51898564+9um4@users.noreply.github.com>
2023-11-28 07:39:22 +0900

    fix: bug

--- main.py
+++ main.py
@@ -21,38 +21,38 @@ while (not piece_index):
     print("이번 게임에 사용할 말을 정해 주세요.")
     piece_index = int(input())
 
-player_index: int = game.register_player(nickname, piece_index, 0, 0) != -1
+player_index: int = game.register_player(nickname, piece_index, 0, 0)
 
-print(player_index)
-
-if (player_index != -1):
-    piece = Knight(modi.MODI())
-    while piece.__is_alive__:
-        state = game.spread_reader.get_state(player_index)
-        if state == "MY_TURN":
+piece = Knight(modi.MODI())
+while piece.__is_alive__ and player_index != -1:
+    if (player_index == 1):
+        if (game.spread_reader.first_player['state'] == "MY_TURN"):
             if (piece.__display__.text != "My turn!"):
                 piece.__display__.text = "My turn!"
-            command = game.spread_reader.
+            command = game.spread_reader.first_player['command']
             if (command != 0):
-                piece.move(int(command))
-                game.spread_reader.set_command(player_index, None)
-                game.spread_reader.set_state(player_index, "OPPONENT_TURN")
-                game.spread_reader.set_state(3 - player_index, "MY_TURN")
+                piece.move(command)
+                game.spread_reader.first_player['command'] = 0
+                game.spread_reader.first_player['state'] = "OPPONENT_TURN"
+                game.spread_reader.second_player['state'] = "MY_TURN"
             sleep(2)
-
-        elif state == "OPPONENT_TURN":
-            if (piece.__display__.text != "Opponent's turn!"):
-                piece.__display__.text = "Opponent's turn!"
-            for _ in range(0, 200):
-                sleep(0.01)
-                piece.__detect_die__()
-
-        else:
-            if (piece.__display__.text != "Waiting for other player..."):
-                piece.__display__.text = "Waiting for other player..."
+    elif (player_index == 2):
+        if (game.spread_reader.second_player['state'] == "MY_TURN"):
+            if (piece.__display__.text != "My turn!"):
+                piece.__display__.text = "My turn!"
+            command = game.spread_reader.second_player['command']
+            if (command != 0):
+                piece.move(command)
+                game.spread_reader.second_player['command'] = 0
+                game.spread_reader.second_player['state'] = "OPPONENT_TURN"
+                game.spread_reader.first_player['state'] = "MY_TURN"
             sleep(2)
+    else:
+        if (piece.__display__.text != "Waiting for other player..."):
+            piece.__display__.text = "Waiting for other player..."
+        sleep(2)
 
-else:
+if (player_index == -1):
     print("게임을 실행할 수 없습니다!")
 
 print("Program Terminated.")

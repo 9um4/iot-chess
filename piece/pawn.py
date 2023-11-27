fe7fca8aa38aad27ef0a9ac3d647a303acfe51e6
9um4 <51898564+9um4@users.noreply.github.com>
2023-11-28 05:49:37 +0900

    fix: check if pawn was moved

--- piece/pawn.py
+++ piece/pawn.py
@@ -13,16 +13,10 @@ class Pawn(Piece):
         super().__init__(bundle, column_time, degree_time)
         self.is_moved_before: bool = False
 
-        self.movement: Dict[int, Dict[str, any]] = {
-            1: {
-                "function": lambda: self.move_forward(1),
-                "expression": "한 칸 앞으로 움직입니다."
-            },
-            2: {
-                "function": lambda: self.move_forward(1) if self.is_moved_before else self.move_forward(2) ,
-                "expression": "과거에 움직이지 않은 경우, 두 칸 앞으로 움직입니다."
-            }
-        }
-
     def move(self, movement_index: int = 1):
-        self.movement[movement_index]["function"]
+        if (movement_index == 1):
+            self.move_forward(1)
+            self.is_moved_before = True
+        elif (movement_index == 2):
+            self.move_forward(1) if self.is_moved_before else self.move_forward(2)
+            self.is_moved_before = True

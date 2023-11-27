ca1789d4e719dffd1d9b5ee673a2502a9944514a
9um4 <51898564+9um4@users.noreply.github.com>
2023-11-28 05:46:41 +0900

    update: knight logic simplized

--- piece/knight.py
+++ piece/knight.py
@@ -1,5 +1,7 @@
 from modi import MODI
 
+from typing import Dict
+
 if __name__ == "__main__":
     from piece import Piece
 else:
@@ -8,7 +10,7 @@ else:
 class Knight(Piece):
 
     def __init__(self, bundle: MODI, column_time: float = 1.0, degree_time: float = 1.02):
-        super().__init(bundle, column_time, degree_time)
+        super().__init__(bundle, column_time, degree_time)
         
     def move(self, movement: int = 1):
         if (movement == 1):
